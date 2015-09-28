3bb0cfb6b98e69a4bb7b3beab88ec78bbf5dd253
James White <cyberyt@gmail.com>
2015-09-28 20:36:48 +0100

    index and create work on Users

---
+++ controllers/users.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var usersRoute = express.Router();
+var moongoose = require('mongoose');
+
+// To use JSON etc.
+var bodyParser = require('body-parser');
+usersRoute.use(bodyParser.urlencoded({ extended: false }));
+usersRoute.use(bodyParser.json())
+
+//MODEL
+var User = require('../models/user')
+
+//(index, show, create, update and delete)
+// INDEX
+usersRoute.get('/', function(req, res) {
+  User.find({}, function(err, users) {
+    if (err) console.log(err);
+    res.json(users)
+  })
+});
+
+// SHOW
+usersRoute.get('/:id', function(req, res) {
+  console.log(req.params.id)
+  User.findById(req.params.id, function(err, user) {
+    if (err) console.log(err);
+    res.json(user)
+  })
+});
+
+// CREATE
+usersRoute.post('/', function(req, res){
+  console.log('create user route');
+  console.log(req.body);
+  var params = req.body;
+  var newUser = new User({
+    name: params.name,
+    gender: params.gender,
+    dob: params.date
+  })
+  newUser.save(function (err, user) {
+    if(err) console.log(err);
+    console.log('User has been created!');
+  res.json(user);
+  });
+})
+
+// UPDATE
+
+
+// DELETE
+
+module.exports = usersRoute;
